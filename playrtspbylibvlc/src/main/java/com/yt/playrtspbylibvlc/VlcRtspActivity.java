59a03ab9fab5a6ca48bc17fdd8cbd1afbf87a790
qiu <851723781@qq.com>
2024-04-17 11:07:45 +0800

    new :增加使用libvlc 播放视频流模块

---
+++ playrtspbylibvlc/src/main/java/com/yt/playrtspbylibvlc/VlcRtspActivity.java
@@ -0,0 +1,14 @@
+package com.yt.playrtspbylibvlc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VlcRtspActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vlc_rtsp);
+    }
+}
